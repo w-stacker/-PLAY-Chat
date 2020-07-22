012dc07db3e47fe43d277f1db1d214523b1fd0f4
William Spigner <senditomy@gmail.com>
2020-07-22 02:54:42 -0700

    update

---
+++ server.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, 
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+ } = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = '!PLAYChat Bot';
+
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+      const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room)
+
+  //Welcome message
+  socket.emit('message', formatMessage(botName,'Welcome to !PLAY-Chat!'));
+
+  //when a user connects
+  socket.broadcast
+  .to(user.room)
+  .emit('message', 
+  formatMessage(botName, `${user.username} has joined the chat`));
+
+  //Send user and room info
+  io.to(user.room).emit('roomUsers', {
+    room: user.room,
+    users: getRoomUsers(user.room)
+  });
+
+  });
+
+  //Listen for message
+socket.on('chatMessage', msg => {
+  const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  
+//   //Runs when client disconnect
+// socket.on('disconnect', () => {
+//   io.emit('message', formatMessage(botName,'A user has left the chat'));
+//   });
+//Runs disconnect
+socket.on('disconnect', () => {
+const user = userLeave(socket.id);
+
+if(user) {
+  io.to(user.room).emit('message',
+   formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      //Send user and room info
+  io.to(user.room).emit('roomUsers', {
+    room: user.room,
+    users: getRoomUsers(user.room)
+  });
+
+    }
+
+  });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
